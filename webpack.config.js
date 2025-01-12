57492514e42de6ad46ea7ebee2bb330031f2689a
sarperozkiziltan <sarperozkiziltan@gmail.com>
2025-01-12 22:24:52 +0300

    babel added

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const nodeExternals = require("webpack-node-externals");
 
 module.exports = {
   entry: "./Controller.js",
@@ -8,4 +9,22 @@ module.exports = {
   },
   target: "node",
   mode: "production",
+  externals: [nodeExternals()], // Exclude node_modules from the bundle
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js"],
+  },
 };

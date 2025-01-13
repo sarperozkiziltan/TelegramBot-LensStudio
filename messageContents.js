9bddefd37b4d0e7bfe32c6f178b9a87f56333353
sarperozkiziltan <sarperozkiziltan@gmail.com>
2025-01-13 10:27:42 +0300

    spelling fixed on message contents folder

--- messageContents.js
+++ messageContents.js
@@ -1,11 +1,11 @@
 const HELP = `Hello dear user welcome to the Lens Studio TelegramBot use the Menu button to start using me or just type the commands by hand, here's the commands:"
 ---------------------------------------------------------------------------------------------
-/start --> starts the bot
-/restart --> restarts the bot if somethink goes wrong
-/help --> see this information message again
-/feedback --> send a message/feedback to the creator
+/start --> Starts the bot
+/restart --> Restarts the bot if something goes wrong
+/help --> See this information message again
+/feedback --> Send a message/feedback to the creator
 ---------------------------------------------------------------------------------------------
-PS: no hard feelings you can send your thoughts to the creator but if you say bad words i will ban you :)
+PS: no hard feelings you can send your thoughts to the creator but if you say bad words I will ban you :)
 `;
 
 const RESTART = "Bot restarted successfully";

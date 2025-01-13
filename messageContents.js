996922878aeb0091184200416c039b4bb18ff6bd
sarperozkiziltan <sarperozkiziltan@gmail.com>
2025-01-13 10:11:29 +0300

    message content fixed

--- messageContents.js
+++ messageContents.js
@@ -3,7 +3,7 @@ const HELP = `Hello user welcome to the Lens Studio TelegramBot use the Menu but
 /start --> starts the bot
 /restart --> restarts the bot if somethink goes wrong
 /help --> see this information message again
-/feedback --> send a message/feedback to the developer
+/feedback --> send a message/feedback to the creator
 ---------------------------------------------------------------------------------------------
 PS: no hard feelings you can send your thoughts to the creator but if you say bad words i will ban you :)
 `;
